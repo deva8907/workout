02bf68c599bfc6414446bdb2570ab199cbe6fadd
Devabalan Arumugam <Devabalan.Arumugam@agilysys.com>
2021-09-24 11:55:43 +0530

    Add project files.

---
+++ Kadane.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace workout
+{
+    class Kadane
+    {
+        public static int Solve(int[] nums)
+        {
+            int actualSum = 0, maxSum = Int32.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                actualSum += nums[i];
+                if (actualSum > maxSum)
+                {
+                    maxSum = actualSum;
+                }
+                if (actualSum < 0)
+                {
+                    actualSum = 0;
+                }
+            }
+            return maxSum;
+        }
+    }
+}
---
+++ SpiralOne.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace workout
+{
+    class SpiralOne
+    {
+        public static IList<int> Solve(int[][] matrix)
+        {
+            List<int> result = new List<int>();
+            int iteration = 1;
+            int traversal, nthTraversal;
+            while (true)
+            {
+                traversal = iteration % 4;
+                nthTraversal = iteration / 4;
+                if (traversal == 1 && nthTraversal < matrix[0].Length)
+                {
+                    int i = nthTraversal, j = nthTraversal;
+                    for (; j <= matrix[0].Length - (traversal + nthTraversal); j++)
+                    {
+                        result.Add(matrix[i][j]);
+                    }
+                }
+                else if (traversal == 2 && nthTraversal < matrix.Length)
+                {
+                    int i = 0, j = matrix[0].Length - 1 - nthTraversal;
+                    for (i = nthTraversal + 1; i <= matrix.Length - 1 - nthTraversal; i++)
+                    {
+                        result.Add(matrix[i][j]);
+                    }
+                }
+                else if (traversal == 3 && nthTraversal < matrix[0].Length)
+                {
+                    int i = matrix.Length - 1 - nthTraversal, j;
+                    j = matrix[0].Length - 2 - nthTraversal;
+
+                    for (; j >= nthTraversal; j--)
+                    {
+                        result.Add(matrix[i][j]);
+                    }
+                }
+                else if (traversal == 0 && nthTraversal < matrix[0].Length)
+                {
+                    int i = matrix.Length - nthTraversal - 1; ;
+                    int j = nthTraversal - 1;
+                    for (; i >= nthTraversal; i--)
+                    {
+                        result.Add(matrix[i][j]);
+                    }
+                }
+                if (result.Count == matrix.Length * matrix[0].Length)
+                {
+                    break;
+                }
+                iteration++;
+            }
+            return result;
+        }
+    }
+}
