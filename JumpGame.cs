b6e39127d068c6556830f06ba6a820d5c9331b1b
Devabalan Arumugam <Devabalan.Arumugam@agilysys.com>
2021-09-25 21:28:25 +0530

    adding jump game

---
+++ JumpGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace workout
+{
+    public class JumpGame
+    {
+        public static bool CanJump(int[] nums)
+        {
+            int length = nums.Length;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i; j <= nums[i] && j < length; j++)
+                {
+                    if ((nums[j] + j) >= length - 1)
+                    {
+                        if ((i == length - 1) && (j == length - 1))
+                        {
+                            if ((nums[j] + j == length))
+                            {
+                                return true;
+                            }
+                            return false;
+                        }
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,18 +8,7 @@ namespace workout
     {
         static void Main(string[] args)
         {
-            //spiral(new int[][] { new int[] { 1, 2, 3, 4 }, new int[] { 5, 6, 7, 8 }, new int[] { 9, 10, 11, 12 } });
-            //spiral(new int[][] { new int[] { 1, 2, 3, 4 }, new int[] { 5, 6, 7, 8 }, new int[] { 9, 10, 11, 12 }, new int[] { 13, 14, 15, 16 } });
-            SpiralOne.(new int[][] {
-                new int[] { 1, 2, 3, 4, 5 },
-                new int[] { 6, 7, 8, 9, 10 },
-                new int[] { 11, 12, 13, 14, 15 },
-                new int[] { 16, 17, 18, 19, 20 },
-                new int[] { 21, 22, 23, 24, 25 }
-            });
-            //spiral(new int[][] { new int[] { 1, 2, 3, 4 }});
-            //spiral(new int[][] { new int[] { 1 }, new int[] { 2 }, new int[] { 3 }, new int[] { 4 } });
-            //Console.WriteLine(maxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+            
         }
        
     }
