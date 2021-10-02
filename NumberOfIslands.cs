f9465be9af69049c898077c0426ef7e6aa30c3b3
Devabalan Arumugam <Devabalan.Arumugam@agilysys.com>
2021-10-02 17:40:03 +0530

    count number of islands recursive

---
+++ NumberOfIslands.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace workout
+{
+    public class NumberOfIslands
+    {
+        public static int NumIslands(char[][] grid)
+        {
+            int numberOfIslands = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (IsIsland(grid, i, j))
+                    {
+                        numberOfIslands++;
+                    }
+                }
+            }
+            return numberOfIslands;
+        }
+
+        private static bool IsIsland(char[][] grid, int i, int j)
+        {
+            if (!(i >= 0 && i < grid.Length && j >= 0 && j < grid[i].Length) ||
+                grid[i][j] == '0')
+            {
+                return false;
+            }
+            grid[i][j] = '0';
+            IsIsland(grid, i - 1, j);
+            IsIsland(grid, i, j + 1);
+            IsIsland(grid, i + 1, j);
+            IsIsland(grid, i, j - 1);
+            return true;
+        }
+    }
+}
