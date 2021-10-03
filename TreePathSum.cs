f75b66c0347c34986ae0b2c527ab1405b3fbfc3c
Devabalan Arumugam <Devabalan.Arumugam@agilysys.com>
2021-10-03 17:48:28 +0530

    Tree path sum

---
+++ TreePathSum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace workout
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class TreePathSum
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            return Traverse(root, 0, targetSum);
+        }
+
+        private bool Traverse(TreeNode node, int sumSoFar, int targetSum)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (sumSoFar + node.val == targetSum && node.left == null && node.right == null)
+            {
+                return true;
+            }
+            bool result = Traverse(node.left, sumSoFar + node.val, targetSum);
+            if (!result)
+            {
+                result = Traverse(node.right, sumSoFar + node.val, targetSum);
+            }
+            return result;
+        }
+    }
+}
---
+++ workout.tests/TreeTestFixture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace workout.tests
+{
+    public class TreeTestFixture
+    {
+        [Theory]
+        [ClassData(typeof(TreePathTestData))]
+        public void TreePathSumWorks(TreeNode root, int targetSum, bool expected)
+        {
+            Assert.Equal(expected, new TreePathSum().HasPathSum(root, targetSum));
+        }
+    }
+
+    public class TreePathTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            TreeNode root = new TreeNode(-2, null, new TreeNode(-3));
+            yield return new object[]
+                {
+                    root, -5, true
+                };
+            //root = new TreeNode(1, new TreeNode(2));
+            //yield return new object[]
+            //    {
+            //        root, 1, false
+            //    };
+            
+        }
+
+        private TreeNode ConstructTree(TreeNode node, int?[] input, int i)
+        {
+            if (i < input.Length && input[i].HasValue)
+            {
+                node.val = input[i].Value;
+                node.left = ConstructTree(new TreeNode(), input, 2 * i + 1);
+                node.right = ConstructTree(new TreeNode(), input, 2 * i + 2);
+                return node;
+            }
+            return null;
+        }
+
+        public TreeNode insertLevelOrder(TreeNode root, int?[] input, int i)
+        {
+            // Base case for recursion
+            if (i < input.Length && input[i].HasValue)
+            {
+                TreeNode temp = new TreeNode(input[i].Value);
+                root = temp;
+
+                // insert left child
+                root.left = insertLevelOrder(
+                                root.left, input, 2 * i + 1);
+
+                // insert right child
+                root.right = insertLevelOrder(
+                                root.right, input, 2 * i + 2);
+            }
+            return root;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
