6d18b4ad2a161666fee2d12dfc20f4141db99fc8
Devabalan Arumugam <Devabalan.Arumugam@agilysys.com>
2021-10-04 11:56:05 +0530

    tree path sum backtracking

---
+++ TreePathSumNodes.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace workout
+{
+    public class TreePathSumNodes
+    {
+        public IList<IList<int>> PathSum(TreeNode root, int targetSum)
+        {
+            if (root == null)
+            {
+                return new List<IList<int>>();
+            }
+            Stack<Tuple<TreeNode, List<int>>> treeNodes = new Stack<Tuple<TreeNode, List<int>>>();
+            IList<IList<int>> output = new List<IList<int>>();
+            treeNodes.Push(new Tuple<TreeNode, List<int>>(root, new List<int>() { root.val }));
+            TreeNode topNode;
+            IList<int> path;
+            while (treeNodes.TryPop(out Tuple<TreeNode, List<int>> element))
+            {
+                topNode = element.Item1;
+                path = element.Item2;
+                if (topNode.left == null && topNode.right == null && path.Sum() == targetSum)
+                {
+                    output.Add(path);
+                }
+                if (topNode.left != null)
+                {
+                    var leftPath = new List<int>(path)
+                    {
+                        topNode.left.val
+                    };
+                    treeNodes.Push(new Tuple<TreeNode, List<int>>(topNode.left, leftPath));
+                }
+                if (topNode.right != null)
+                {
+                    var rightPath = new List<int>(path)
+                    {
+                        topNode.right.val
+                    };
+                    treeNodes.Push(new Tuple<TreeNode, List<int>>(topNode.right, rightPath));
+                }
+            }
+            return output;
+        }
+        private static List<IList<int>> output;
+        public IList<IList<int>> PathSumRecursive(TreeNode root, int targetSum)
+        {
+            if (root == null)
+            {
+                return new List<IList<int>>();
+            }
+            output = new List<IList<int>>();
+            Traverse(root, targetSum, new List<int> { root.val });
+            return output;
+        }
+        private void Traverse(TreeNode node, int targetSum, List<int> path)
+        {
+            if (path.Sum() == targetSum && node.left == null && node.right == null)
+            {
+                output.Add(path);
+            }
+            if (node.left != null)
+            {
+                var leftPath = new List<int>(path);
+                leftPath.Add(node.left.val);
+                Traverse(node.left, targetSum, leftPath);
+            }
+            if (node.right != null)
+            {
+                var rightPath = new List<int>(path);
+                rightPath.Add(node.right.val);
+                Traverse(node.right, targetSum, rightPath);
+            }
+        }
+    }
+}
--- workout.tests/TreeTestFixture.cs
+++ workout.tests/TreeTestFixture.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -13,6 +14,83 @@ namespace workout.tests
         public void TreePathSumWorks(TreeNode root, int targetSum, bool expected)
         {
             Assert.Equal(expected, new TreePathSum().HasPathSum(root, targetSum));
+        }
+        [Theory]
+        [ClassData(typeof(TreePathSumNodesTestData))]
+        public void TreePathNodeWorks(TreeNode root, int targetSum, List<List<int>> expected)
+        {
+            new TreePathSumNodes().PathSumRecursive(root, targetSum);
+        }
+    }
+
+    public class TreePathSumNodesTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            TreeNode root = CreateTree(new int?[] { 5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1 });
+            yield return new object[]
+                {
+                    root, 22, new List<List<int>>()
+                    {
+                        new List<int>(){ 5, 4, 11, 2 },
+                        new List<int>(){ 5, 8, 4, 5 }
+                    }
+                };
+            root = CreateTree(new int?[] { 1, 2, 3 });
+            yield return new object[]
+                {
+                    root, 5, new List<List<int>>()
+                };
+            root = CreateTree(new int?[] { 1, 2 });
+            yield return new object[]
+                {
+                    root, 0, new List<List<int>>()
+                };
+
+
+            yield return new object[]
+                {
+                    CreateTree(new int?[] { }), 1, new List<List<int>>()
+                };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        public static TreeNode CreateTree(int?[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                return null;
+            }
+
+            Queue<TreeNode> treeNodeQueue = new Queue<TreeNode>();
+            Queue<int?> integerQueue = new Queue<int?>();
+            for (int i = 1; i < array.Length; i++)
+            {
+                integerQueue.Enqueue(array[i]);
+            }
+
+            TreeNode treeNode = new TreeNode(array[0].Value);
+            treeNodeQueue.Enqueue(treeNode);
+
+            while (integerQueue.Any())
+            {
+                int? leftVal = !integerQueue.Any() ? null : integerQueue.Dequeue();
+                int? rightVal = !integerQueue.Any() ? null : integerQueue.Dequeue();
+                TreeNode current = treeNodeQueue.Dequeue();
+                if (leftVal.HasValue)
+                {
+                    TreeNode left = new TreeNode(leftVal.Value);
+                    current.left = left;
+                    treeNodeQueue.Enqueue(left);
+                }
+                if (rightVal.HasValue)
+                {
+                    TreeNode right = new TreeNode(rightVal.Value);
+                    current.right = right;
+                    treeNodeQueue.Enqueue(right);
+                }
+            }
+            return treeNode;
         }
     }
 
@@ -30,7 +108,7 @@ namespace workout.tests
             //    {
             //        root, 1, false
             //    };
-            
+
         }
 
         private TreeNode ConstructTree(TreeNode node, int?[] input, int i)
