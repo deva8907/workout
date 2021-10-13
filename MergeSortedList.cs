4637e871c462153a840f6d6439fdd46374493225
Devabalan Arumugam <Devabalan.Arumugam@agilysys.com>
2021-10-13 18:03:28 +0530

    merge list with merge sort implementation

---
+++ MergeSortedList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace workout
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class MergeSortedList
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            if (lists == null || lists.Length == 0)
+            {
+                return null;
+            }
+            return MergeList(lists, 0, lists.Length - 1);
+        }
+
+        private ListNode MergeList(ListNode[] lists, int start, int end)
+        {
+            if (end == start)
+            {
+                return lists[start];
+            }
+            int middle = ((end - start) / 2) + start;
+            var leftList = MergeList(lists, start, middle);
+            var rightList = MergeList(lists, middle + 1, end);
+            return Merge(leftList, rightList);
+        }
+
+        private ListNode Merge(ListNode leftList, ListNode rightList)
+        {
+            if (leftList == null && rightList == null)
+            {
+                return null;
+            }
+            ListNode left = leftList, right = rightList, result = new ListNode();
+            ListNode first = result;
+            while (true)
+            {
+                if (left != null && right != null)
+                {
+                    if (left.val <= right.val)
+                    {
+                        result.val = left.val;
+                        left = left.next;
+                    }
+                    else
+                    {
+                        result.val = right.val;
+                        right = right.next;
+                    }
+                }
+                else if (left == null && right != null)
+                {
+                    result.val = right.val;
+                    right = right.next;
+                }
+                else if (right == null && left != null)
+                {
+                    result.val = left.val;
+                    left = left.next;
+                }
+                if (left == null && right == null)
+                {
+                    break;
+                }
+                else
+                {
+                    result.next = new ListNode();
+                    result = result.next;
+                }
+            }
+            return first;
+        }
+    }
+}
--- workout.tests/SortFixture.cs
+++ workout.tests/SortFixture.cs
@@ -14,6 +14,56 @@ namespace workout.tests
         {
             MergeSortedArray.Solve(input);
         }
+        [Theory]
+        [ClassData(typeof(MergeSortedListTestData))]
+        public void TestMergeLists(ListNode[] listNodes)
+        {
+            new MergeSortedList().MergeKLists(listNodes);
+        }
+    }
+
+    internal class MergeSortedListTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            int[][] input = new int[][]
+            {
+                new int[] { 1, 4, 5 },
+                new int[] { 1, 3, 4},
+                new int[]{ 2, 6}
+            };
+            ListNode[] listNode;
+            //listNode = ToListNode(input);
+            //yield return new object[] { listNode };
+            
+            yield return new object[] { new ListNode[] { null, null } };
+        }
+
+        private ListNode[] ToListNode(int[][] input)
+        {
+            ListNode[] nodeArray = new ListNode[input[0].Length];
+            ListNode listNode;
+            ListNode first;
+            for (int j = 0; j < input.Length; j++)
+            {
+                listNode = new ListNode();
+                first = listNode;
+                var array = input[j];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    listNode.val = array[i];
+                    if (i != array.Length - 1)
+                    {
+                        listNode.next = new ListNode();
+                        listNode = listNode.next;
+                    }
+                }
+                nodeArray[j] = first;
+            }
+            return nodeArray;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 
     public class MergeArrayTestData : IEnumerable<object[]>
