44c2246a40ec8688368cf5070014625b1d0e5131
Devabalan Arumugam <darumugam@lenovo.com>
2024-06-13 09:16:12 +0530

    Add message truncate solution

---
+++ StringTruncate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace workout
+{
+    public class StringTruncate
+    {
+        public static string Solution(string message, int K)
+        {
+            if (message.Length <= K)
+            {
+                return message;
+            }
+
+            var words = message.Split(' ');
+            int allowedLength = K - 4;
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var word in words)
+            {
+                if (allowedLength - (1 + word.Length) > -2)
+                {
+                    allowedLength = allowedLength - 1 - word.Length;
+                    stringBuilder.Append(word);
+                    stringBuilder.Append(" ");
+                    continue;
+                }
+                break;
+            }
+            stringBuilder.Append("...");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ workout.tests/StringTruncateTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace workout.tests
+{
+    public class StringTruncateTests
+    {
+        [Theory]
+        [InlineData("There is an animal with four legs", 15, "There is an ...")]
+        [InlineData("Hello there", 5, "...")]
+        [InlineData("I saw an unicorn", 16, "I saw an unicorn")]
+        public void ShouldTruncateMessage(string message, int limit, string expected)
+        {
+            Assert.Equal(expected, StringTruncate.Solution(message, limit));
+        }
+    }
+}
